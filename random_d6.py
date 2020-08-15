07a86968ca9feab45c4b7feaf9eb579ff44ed33f
Gus <gusrobins60@gmail.com>
2020-08-14 19:02:52 -0700

    first commit

---
+++ random_d6.py
@@ -0,0 +1,22 @@
+import random
+
+
+class DiceRoll:
+	def __init__(self,dice):
+		self.dice = dice
+
+	def d6(self):
+		self.min_roll = 1
+		self.max_roll = 6
+
+		dice = random.rantint(min_roll,max_roll)
+
+		print(dice)
+
+
+my_dice = DiceRoll(dice)
+my_dice.d6()
+
+
+
+
